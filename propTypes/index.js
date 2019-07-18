ef214b199bb7463ea2321f5ab31b3ab0729f0d28
abc <rajat2502@users.noreply.github.com>
2019-07-18 19:00:55 +0530

    learned about propTypes

---
+++ propTypes/index.js
@@ -0,0 +1,55 @@
+const GUINEAPATHS = [
+  'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-guineapig-1.jpg',
+  'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-guineapig-2.jpg',
+  'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-guineapig-3.jpg',
+  'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-guineapig-4.jpg'
+];
+
+class GuineaPigsContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { currentGP: 0 };
+  }
+
+  nextGP() {
+    let current = this.state.currentGP;
+    let next = ++current % GUINEAPATHS.length;
+    this.setState({ currentGP: next });
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(this.nextGP, 5000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    let src = GUINEAPATHS[this.state.currentGP];
+    return <GuineaPigs src={src} />;
+  }
+};
+
+class GuineaPigs extends React.Component {
+  render() {
+    let src = this.props.src;
+    return (
+      <div>
+        <h1>Cute Guinea Pigs</h1>
+        <img src={src} />
+      </div>
+    );
+  }
+}
+
+// proptype declaration
+// GuineaPigs.propTypes = {
+//   src: React.PropTypes.string.isRequired
+// } 
+
+ReactDOM.render(
+  <GuineaPigsContainer />, 
+  document.getElementById('app')
+);
